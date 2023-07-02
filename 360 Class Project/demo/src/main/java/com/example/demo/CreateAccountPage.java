95f928fcda66bfbf8fe4a9f53a1312a456441da4
Gage Cates <gage.cates01@gmail.com>
2023-07-01 17:53:23 -0700

    Created The User Interface for CreateAccountPage class and was able to add resources folder so we can add images in runtime.

--- 360 Class Project/demo/src/main/java/com/example/demo/CreateAccountPage.java
+++ 360 Class Project/demo/src/main/java/com/example/demo/CreateAccountPage.java
@@ -1,9 +1,14 @@
 package com.example.demo;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 public class CreateAccountPage
 {
@@ -17,30 +22,112 @@ public class CreateAccountPage
 
     public void show()
     {
-        VBox layout = createLayout();
-        layout.setAlignment(Pos.CENTER);
-        Scene scene = new Scene(layout, 300, 200);
-        Label createAccountLabel = new Label("Create Account");
-        layout.getChildren().add(createAccountLabel);
-        stage.setScene(scene);
-        layout.requestFocus();
+        stage.setScene(createLayout());
         stage.show();
     }
 
-    private VBox createLayout()
+    private Scene createLayout()
     {
-        VBox layout = new VBox(10);
+        VBox leftLayout = new VBox();
+        VBox infoLayout = new VBox(10);
+        StackPane finalLayout = new StackPane();
 
         // Temporary back button that takes you to Login Page
-        Button back = new Button("Temporary Back Button");
-        back.setStyle("-fx-background-color: #E2CE15; -fx-background-radius: 5");
-        back.setMinWidth(100);
+        Button back = new Button("X");
+        back.setStyle("-fx-background-color: #eaeaea; -fx-background-radius: 10; -fx-font-size: 20; -fx-alignment: center;");
         back.setOnAction(e -> {
             LoginPage loginPage = new LoginPage(stage);
             loginPage.show();
         });
 
-        layout.getChildren().addAll(back);
-        return layout;
+        //Spacer Label
+        Label spacer = new Label(" ");
+        spacer.setFont(Font.font(30));
+
+        //Label for instructions on filling out information
+        Label welcomeLabel1 = new Label("Your next step with Healfy");
+        welcomeLabel1.setFont(Font.font("Arial", FontWeight.BLACK, 30));
+        welcomeLabel1.setTextFill(Color.web("#9741A5"));
+
+        Label welcomeLabel2 = new Label("Please enter all information below to get started");
+        welcomeLabel2.setTextFill(Color.web("#9741A5"));
+
+        //Label and text field for first name.
+        Label firstnameLabel = new Label("First Name:");
+        firstnameLabel.setTextFill(Color.web("#9741A5"));
+
+        TextField firstnameField = new TextField();
+        firstnameField.setBackground(new Background(new BackgroundFill(Color.web("#ECECEC"), new CornerRadii(10.0), Insets.EMPTY)));
+        firstnameField.setMaxWidth(250);
+
+        //Label and text field for last name.
+        Label lastnameLabel = new Label("Last Name:");
+        lastnameLabel.setTextFill(Color.web("#9741A5"));
+
+        TextField lastnameField = new TextField();
+        lastnameField.setBackground(new Background(new BackgroundFill(Color.web("#ECECEC"), new CornerRadii(10.0), Insets.EMPTY)));
+        lastnameField.setMaxWidth(250);
+
+        //Label and Date Picker for Date of birth.
+        Label dateLabel = new Label("Date Of Birth:");
+        dateLabel.setTextFill(Color.web("#9741A5"));
+
+        DatePicker datePicker = new DatePicker();
+        datePicker.setStyle("-fx-control-inner-background: #ECECEC; -fx-max-width: 250; -fx-background-color: #ECECEC");
+
+        //Label and text field for phone number.
+        Label phoneLabel = new Label("Phone Number:");
+        phoneLabel.setTextFill(Color.web("#9741A5"));
+
+        TextField phoneField = new TextField();
+        phoneField.setBackground(new Background(new BackgroundFill(Color.web("#ECECEC"), new CornerRadii(10.0), Insets.EMPTY)));
+        phoneField.setMaxWidth(250);
+
+        //Label and text field for username
+        Label usernameLabel = new Label("Username:");
+        usernameLabel.setTextFill(Color.web("#9741A5"));
+
+        TextField usernameField = new TextField();
+        usernameField.setBackground(new Background(new BackgroundFill(Color.web("#ECECEC"), new CornerRadii(10.0), Insets.EMPTY)));
+        usernameField.setMaxWidth(250);
+
+        //Label and text field for password
+        Label passwordLabel = new Label("Password:");
+        passwordLabel.setTextFill(Color.web("#9741A5"));
+
+        TextField passwordField = new TextField();
+        passwordField.setBackground(new Background(new BackgroundFill(Color.web("#ECECEC"), new CornerRadii(10.0), Insets.EMPTY)));
+        passwordField.setMaxWidth(250);
+
+        //Create account button.
+        Button createAccountButton = new Button("Create Account");
+        createAccountButton.setStyle("-fx-background-color: #E2CE15; -fx-background-radius: 5");
+        createAccountButton.setMinWidth(100);
+        createAccountButton.setOnAction(e -> {
+            LoginPage loginPage = new LoginPage(stage);
+            loginPage.show();
+        });
+
+        //Logo Image
+        ImageView imageView = new ImageView(new Image("Healfy-Logo.png"));
+        imageView.setFitWidth(400);
+        imageView.setPreserveRatio(true);
+
+        //Creation of gathering all the information layout.
+        infoLayout.getChildren().addAll(firstnameLabel, firstnameField, lastnameLabel, lastnameField, dateLabel,
+                datePicker, phoneLabel, phoneField, usernameLabel, usernameField, passwordLabel, passwordField,
+                createAccountButton);
+
+        //Creation of the complete left side of scene.
+        leftLayout.getChildren().addAll(welcomeLabel1, welcomeLabel2, spacer, infoLayout);
+
+        //Completing the whole scene.
+        finalLayout.getChildren().addAll(leftLayout, back, imageView);
+        StackPane.setAlignment(leftLayout, Pos.CENTER);
+        StackPane.setAlignment(back, Pos.TOP_RIGHT);
+        StackPane.setAlignment(imageView, Pos.CENTER_RIGHT);
+        finalLayout.setPadding(new Insets(20));
+        Scene scene = new Scene(finalLayout, 300, 200);
+        return scene;
     }
 }
