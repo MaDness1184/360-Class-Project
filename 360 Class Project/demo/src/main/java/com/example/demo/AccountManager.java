2ecff4029d99dff66ec2f40aae7cccde144ff79b
Ryan Carpenter <dev.ryan1184@gmail.com>
2023-07-01 21:44:16 -0700

    static AccountManager Class

--- 360 Class Project/demo/src/main/java/com/example/demo/AccountManager.java
+++ 360 Class Project/demo/src/main/java/com/example/demo/AccountManager.java
@@ -1,11 +1,11 @@
 package com.example.demo;
 import java.util.List;
 public class AccountManager {
-    private List<Account> accounts;
-    public Account activeAccount = null;
+    private static List<Account> accounts;
+    public static Account activeAccount = null;
     // PatientRecord newRecord = null;
 
-    public Account AccountSearch(int ID)    // Search for an account using an id
+    public static Account AccountSearch(int ID)    // Search for an account using an id
     {
         for (Account account : accounts) {      // for each loop to loop through list
             if (account.getID() == ID)
@@ -14,17 +14,17 @@ public class AccountManager {
         return null;                        // return NULL if the account was not found
     }
 
-    public void SetActiveAccount(Account account)
+    public static void SetActiveAccount(Account account)
     {
         activeAccount = account;
     }
 
-    public void AddAccount(Account newAccount)
+    public static void AddAccount(Account newAccount)
     {
         accounts.add(newAccount);
     }
 
-    public void DeleteAccount(int ID)
+    public static void DeleteAccount(int ID)
     {
         accounts.removeIf(account -> account.getID() == ID);
     }
